109e5e7d4333f5250aea14b1b4e8969c74ca4eda
thisisbankole <bjamgbadi@gmail.com>
2023-10-06 04:12:47 +0100

    up

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ app = connex_app.app
 
 # This tells SQLAlchemy to use SQLite as the database and a file named shopa.db in the current directory as the database file.
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(BASE_DIR, 'shopa.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(BASE_DIR,'tmp', 'shopa.db')
 
 #app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///shopa.db'
 
