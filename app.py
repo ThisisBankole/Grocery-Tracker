df79f03be5b131dbdd06d6612bfcba6fcd2ea5fa
thisisbankole <bjamgbadi@gmail.com>
2023-10-08 16:13:07 +0100

    pmi

--- app.py
+++ app.py
@@ -301,7 +301,7 @@ def inject_datetime():
       
 
 if __name__ == "__main__":
-   #init_db()
+   init_db()
    port = int(os.environ.get("PORT", 8000))
    app.run(host="0.0.0.0", port=port, debug=True)
     
